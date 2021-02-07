f234015b3eb91f4f7e42e85544443db1d576404e
Tony Arkles <tony@persea.ca>
2021-02-06 21:26:07 -0600

    Loading a certificate and serving a TLS connection

--- grissom.lisp
+++ grissom.lisp
@@ -7,11 +7,21 @@
 (defvar *listen-server* nil)
 
 (defun connection-handler (stream)
-  (declare (type stream stream))
-  (write-string "hello" stream))
+  ;; (declare (type stream stream))
+  (let* ((tls-stream (cl+ssl:make-ssl-server-stream stream
+                                                    :certificate "cert.pem"
+                                                    :key "key.pem")))
+    
+    (write-sequence (babel:string-to-octets (format nil "hello~%") :encoding :utf-8) tls-stream)
+    (finish-output tls-stream)))
 
 (defun start-server ()
   (let ((port 4567))
-    (setq *listen-server* (usocket:socket-server "0.0.0.0" port 'connection-handler))
+    (setq *listen-server*
+          (usocket:socket-server
+           "0.0.0.0" port
+           'connection-handler ()
+           :element-type '(unsigned-byte 8)
+           :multi-threading t))
     nil))
 
